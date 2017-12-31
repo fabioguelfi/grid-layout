008203f432a4ed343416a220de02d4867463c6f6
Suguru Inatomi <suguru.inatomi@gmail.com>
2017-12-31 13:52:08 +0900

    Implement lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+const _template = require('./app.component.html');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  get template() {
+    return _template;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GridLayoutModule } from './modules/grid-layout/grid-layout.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
